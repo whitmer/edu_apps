4c8849ebf66c468f04d56b87b8075c5064d142d3
Brian Whitmer <brian@instructure.com>
2012-03-24 17:04:15 -0600

    bumpin round 3

--- lti_example.rb
+++ lti_example.rb
@@ -306,16 +306,18 @@ post "/bumpin" do
   redirect to("/bumpin/room/#{str}?name=#{params['lis_person_name_full']}")
 end
 
-get "/bumpin/room/:name" do
+get "/bumpin/room/:ids" do
   return <<-HTML
   <html>
   <body>
-    <script type="text/javascript" src="http://www.bumpin.com/web_widget/users/bar/jquery-1.3.2.min.js"></script><script type="text/javascript" src="http://www.bumpin.com/new_web_widget_js_shoutbox/js_shoutbox_widget.js"></script>
+    <script type="text/javascript" src="http://www.bumpin.com/web_widget/users/swfobject.js"></script>
+    <script type="text/javascript" src="http://www.bumpin.com/web_widget/users/bar/jquery-1.3.2.min.js"></script>
+    <script type="text/javascript" src="http://www.bumpin.com/new_web_widget/widget.js"></script>
     <div id="bumpin-embedded-div" style="width:800px;"><div id="bumpin-widget"></div><div id="bumpin-link-div"><a target="_blank" href="http://www.ticketmy.com/bumpin/index.php">Shoutbox</a></div></div>
-    <script>window.onload =function(){loadNewBumpinJSWidgetWithoutPeoplelist({ height: "500", width: "800", language: "English", color_string: "", color_theme: "default", enable_login: "false", bumpin_policy: "page", bumpin_url: "", widget_title: "Chat Room", enable_guest_login: "true", show_people_list: "true", enable_sound: "false", nick_name: "#{params['name']}" });}</script>
+    <script>window.onload =function(){loadNewBumpinWidget({ height: "500", width: "800", language: "English", color_string: "", color_theme: "default", enable_login: "false", bumpin_policy: "page", bumpin_url: "", widget_title: "Chat Room", enable_guest_login: "true", show_people_list: "true", enable_sound: "false", nick_name: "#{params['name']}" });}</script>
   </body>
   </html>
-  HTML
+HTML
 end
 
 def config_wrap(xml)
