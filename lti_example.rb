bcb867f5fbb35048bf975fe006d1f36a7cd04820
Brian Whitmer <brian@instructure.com>
2012-04-21 15:33:17 -0600

    heroku troubleshoot

--- lti_example.rb
+++ lti_example.rb
@@ -176,7 +176,7 @@ end
 get "/data/lti_examples.jsonp" do
   host = request.scheme + "://" + request.host_with_port
   uri = URI.parse(host + "/data/lti_examples.json")
-  return uri
+  return "uri = " + uri.to_s
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = (request.scheme == 'https')
   request = Net::HTTP::Get.new(uri.path)
