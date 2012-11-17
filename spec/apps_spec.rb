04754bf7a3f48d02a1f9812b4283df77b6941d5d
Brian Whitmer <brian@instructure.com>
2012-11-17 13:45:05 -0700

    spec for API filtering

--- spec/apps_spec.rb
+++ spec/apps_spec.rb
@@ -12,7 +12,7 @@ describe 'Apps API' do
   end
   
   def check_app_response(app)
-    ['image_url', 'big_image_url', 'config_url', 'description', 'name', 'comments_count', 'ratings_count'].each do |key|
+    ['image_url', 'big_image_url', 'description', 'name', 'comments_count', 'ratings_count'].each do |key|
       app[key].should_not be_nil
     end
   end
@@ -27,6 +27,90 @@ describe 'Apps API' do
       json['objects'].length.should == 24
       json['meta']['next'].should == "http://example.org/api/v1/apps?offset=24"
       json['objects'].  each do |obj|
+        check_app_response(obj)
+      end
+    end
+    
+    it "should filter by level" do
+      get '/api/v1/apps?level=K-6th%20Grade'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['meta'].should_not be_nil
+      json['objects'].should_not be_nil
+      json['objects'].length.should > 0
+      json['objects'].  each do |obj|
+        obj['levels'].should be_include('K-6th Grade')
+        check_app_response(obj)
+      end
+      
+      get '/api/v1/apps?level=Bad+List'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['objects'].length.should == 0
+    end
+    
+    it "should filter by category" do
+      get '/api/v1/apps?category=Math'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['meta'].should_not be_nil
+      json['objects'].should_not be_nil
+      json['objects'].length.should > 0
+      json['objects'].  each do |obj|
+        obj['categories'].should be_include('Math')
+        check_app_response(obj)
+      end
+      
+      get '/api/v1/apps?category=Bad+List'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['objects'].length.should == 0
+    end
+    
+    it "should filter by extensions" do
+      get '/api/v1/apps?extension=editor_button'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['meta'].should_not be_nil
+      json['objects'].should_not be_nil
+      json['objects'].length.should > 0
+      json['objects'].  each do |obj|
+        obj['extensions'].should be_include('editor_button')
+        check_app_response(obj)
+      end
+      
+      get '/api/v1/apps?extension=Bad+Extension'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['objects'].length.should == 0
+    end
+    
+    it "should filter by recency" do
+      get '/api/v1/apps?recent=1'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['meta'].should_not be_nil
+      json['objects'].should_not be_nil
+      json['objects'].length.should > 0
+      json['objects'].sort{|a, b| b['added'] <=> a['added'] }.should == json['objects']
+    end
+    
+    it "should filter by platform" do
+      get '/api/v1/apps?platform=Canvas'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['meta'].should_not be_nil
+      json['objects'].should_not be_nil
+      json['objects'].length.should > 0
+      json['objects'].  each do |obj|
+        check_app_response(obj)
+      end
+      
+      get '/api/v1/apps?extension=Bobs+LMS'
+      last_response.should be_ok
+      json = JSON.parse(last_response.body)
+      json['objects'].  each do |obj|
+        obj['extensions'].should be_nil
         check_app_response(obj)
       end
     end
